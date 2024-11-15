c076168aceca17e16ad9fa1e86b87d861b71bfe2
EugeneLys <eugene.lysakov@gmail.com>
2024-11-16 01:07:48 +0500

    SimpleMenu

--- src/main/java/ru/job4j/ood/isp/menu/Printer.java
+++ src/main/java/ru/job4j/ood/isp/menu/Printer.java
@@ -5,25 +5,16 @@ public class Printer implements MenuPrinter {
     @Override
     public void print(Menu menu) {
         for (Menu.MenuItemInfo item : menu) {
-            int level = item.getNumber().length();
+            int length = item.getNumber().length();
             String space = "";
-            if (level == 4) {
+            if (length == 4) {
                 space = "----";
-            } else if (level == 6) {
+            } else if (length == 6) {
                 space = "--------";
-            } else if (level > 6) {
+            } else if (length > 6) {
                 space = "------------";
             }
-            if (level == 6) {
-                System.out.println(space + item.getNumber() + " "
-                        + item.getName());
-            } else if (level == 4) {
-                System.out.println(space + item.getNumber() + " "
-                        + item.getName());
-            } else {
-                System.out.println(space + item.getNumber() + " "
-                        + item.getName());
-            }
+            System.out.println(space + item.getNumber() + " " + item.getName());
         }
     }
 }
